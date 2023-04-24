31dd73c0904e6e60e26a0635e0a29938e0c4e1ec
Adnan_shaikh <adnan.sits.mech@gmail.com>
2023-04-24 09:33:10 +0530

    practice file DSA Topic

---
+++ hashing.py
@@ -0,0 +1,12 @@
+# 1
+hash_value=hash(42)
+print(hash_value)
+
+# 2
+my_dictonary={"apple":1, "banana":2, "orange":3}
+print(my_dictonary)
+
+my_dictonary["guava"]=4
+print(hash("apple"))
+
+print(hash("banana"))
